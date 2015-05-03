1a89e77348258b4a2c9b42aa5fa80c9677ec15b0
realbot <realbot@w3soft.ru>
2015-05-03 21:32:36 +0300

    Написал оценночный тест для DeltaAdd

--- cluster/cluster_test.go
+++ cluster/cluster_test.go
@@ -44,12 +44,40 @@ func TestMoveTransaction(t *testing.T) {
   cluster.Occ(atom.NewAtom("a"))
 }
 
-func TestDeltaAdd(t *testing.T) {
-  _ = []*trn.Transaction{ 
+func TestDeltaAddEvaluative(t *testing.T) {
+  r := 2.6
+
+  trans := []*trn.Transaction{
     trn.BuildTransaction([]string{ "a", "b" }),
     trn.BuildTransaction([]string{ "a", "b", "c" }),
     trn.BuildTransaction([]string{ "a", "c", "d" }),
     trn.BuildTransaction([]string{ "d", "e" }),
     trn.BuildTransaction([]string{ "d", "e", "f" }),
+  }
+
+  clusters := []*Cluster{ AddCluster(),
+    AddCluster(), AddCluster(), AddCluster() }
+
+  clusters[0].MoveTransaction(trans[0])
+  clusters[0].MoveTransaction(trans[1])
+  clusters[0].MoveTransaction(trans[2])
+  clusters[1].MoveTransaction(trans[3])
+  clusters[1].MoveTransaction(trans[4])
+  clusters[2].MoveTransaction(trans[0])
+  clusters[2].MoveTransaction(trans[1])
+  clusters[3].MoveTransaction(trans[2])
+  clusters[3].MoveTransaction(trans[3])
+  clusters[3].MoveTransaction(trans[4])
+
+  if clusters[0].DeltaAdd(trans[0], r) <= clusters[3].DeltaAdd(trans[0], r) {
+    t.Error("Bad DeltaAdd for first compare")
+  }
+
+  if clusters[3].DeltaAdd(trans[3], r) <= clusters[2].DeltaAdd(trans[3], r) {
+    t.Error("Bad DeltaAdd for second compare")
+  }
+
+  if clusters[2].DeltaAdd(trans[2], r) <= clusters[3].DeltaAdd(trans[2], r) {
+    t.Error("Bad DeltaAdd for third compare")
   }
 }
