c85ead3efe5fa40d5cf22dc5743c436fca8716cb
realbot <realbot@w3soft.ru>
2015-05-05 20:32:29 +0300

    Написал тесты для IO

--- io/io.go
+++ io/io.go
@@ -4,33 +4,47 @@ import (
   "github.com/realb0t/go-clope/transaction"
 )
 
+// Интерфейс Ввода
 type Input interface {
   Next() *transaction.Transaction
 }
 
+// Интерфейс Вывода
 type Output interface {
   Input
   Write(*transaction.Transaction)
 }
 
+// Тестовый Ввода (test input)
 type MemoryInput struct {
   data []*transaction.Transaction
 }
 
-type MemoryOutput struct {
-  Data []*transaction.Transaction
-  index int
+// Создать новый тестовый Ввод
+func NewMemoryInput(data []*transaction.Transaction) *MemoryInput {
+  return &MemoryInput{data}
 }
 
+// Тестовый Вывод (test output)
+type MemoryOutput struct {
+  Data []*transaction.Transaction
+}
+
+// Создать новый тестовый Вывод
+func NewMemoryOutput() *MemoryOutput {
+  return &MemoryOutput{[]*transaction.Transaction{}}
+}
+
+// Извлечь следующее значение из Ввода
 func (r *MemoryInput) Next() *transaction.Transaction {
   var trans *transaction.Transaction
   trans, r.data = r.data[len(r.data)-1], r.data[:len(r.data)-1]
   return trans
 }
 
+// Извлечь следующее значение из Вывода
 func (r *MemoryOutput) Next() *transaction.Transaction {
-  if r.index > 0 {
-    r.index--
+  if len(r.Data) > 0 {
     var trans *transaction.Transaction
     trans, r.Data = r.Data[len(r.Data)-1], r.Data[:len(r.Data)-1]
     return trans
@@ -39,7 +53,7 @@ func (r *MemoryOutput) Next() *transaction.Transaction {
   }
 }
 
+// Записать значение в ввод и сбросить счетчик извлечения значений
 func (r *MemoryOutput) Write(trans *transaction.Transaction) {
   r.Data = append(r.Data, trans)
-  r.index = len(r.Data)
 }
---
+++ io/io_test.go
@@ -0,0 +1,63 @@
+package io
+
+import (
+  "testing"
+  a "github.com/realb0t/go-clope/atom"
+  tr "github.com/realb0t/go-clope/transaction"
+)
+
+func TestNewMemoryInput(t *testing.T) {
+  _ = NewMemoryInput([]*tr.Transaction{})
+}
+
+func TestNewMemoryOutput(t *testing.T) {
+  _ = NewMemoryOutput()
+}
+
+func TestMemoryInputNext(t *testing.T) {
+  trans := []*tr.Transaction{ 
+    tr.NewTransaction(a.NewAtoms([]string{ "a", "b" })),
+    tr.NewTransaction(a.NewAtoms([]string{ "c" })),
+  }
+  input := NewMemoryInput(trans)
+  nextTran := input.Next()
+  if nextTran != trans[1] {
+    t.Error("Next transaction", nextTran, " != ", trans[1] )
+  }
+}
+
+func TestMemoryOutputWrite(t *testing.T) {
+  trans := []*tr.Transaction{ 
+    tr.NewTransaction(a.NewAtoms([]string{ "a", "b" })),
+    tr.NewTransaction(a.NewAtoms([]string{ "c" })),
+  }
+  output := NewMemoryOutput()
+
+  if output.Next() != nil {
+    t.Error("Not empty first Next value")
+  }
+
+  output.Write(trans[0])
+  output.Write(trans[1])
+
+  if len(output.Data) != 2 {
+    t.Error("Not correct data size")
+  }
+
+  fNextTran := output.Next()
+  if fNextTran != trans[1] {
+    t.Error("Next transaction", fNextTran, " != ", trans[1] )
+  }
+
+  sNextTran := output.Next()
+  if sNextTran != trans[0] {
+    t.Error("Next transaction", sNextTran, " != ", trans[0] )
+  }
+
+  output.Write(fNextTran)
+  output.Write(sNextTran)
+
+  if len(output.Data) != 2 {
+    t.Error("Not correct data size")
+  }
+}
