c3750dd52585139da08b572f6fa93e8da359c987
realbot <realbot@w3soft.ru>
2015-05-05 20:54:59 +0300

    Добавил тест на конструктор для Clope

--- clope/clope_test.go
+++ clope/clope_test.go
@@ -2,8 +2,15 @@ package clope
 
 import (
   "testing"
+  "github.com/realb0t/go-clope/io"
+  a "github.com/realb0t/go-clope/atom"
+  tr "github.com/realb0t/go-clope/transaction"
 )
 
 func TestNewProcess(t *testing.T) {
-  //_ = NewProcess(make([]*atom.Atom, 0))
+  trans := []*tr.Transaction{
+    tr.NewTransaction(a.NewAtoms([]string{ "a" })),
+  }
+  _ = NewProcess(io.NewMemoryInput(&trans),
+    io.NewMemoryOutput(), 1.0)
 }
