f4c58b58feba6154392bba2a95fe374b8f52a639
realbot <realbot@w3soft.ru>
2015-05-06 20:41:16 +0300

    Усложнил интеграционный тест

--- clope/clope_test.go
+++ clope/clope_test.go
@@ -5,7 +5,7 @@ import (
   "github.com/realb0t/go-clope/io"
   a "github.com/realb0t/go-clope/atom"
   tr "github.com/realb0t/go-clope/transaction"
-  cl "github.com/realb0t/go-clope/cluster"
+  _ "github.com/realb0t/go-clope/cluster"
 )
 
 func TestNewProcess(t *testing.T) {
@@ -23,10 +23,14 @@ func TestBuildIntegration(t *testing.T) {
     tr.NewTransaction(a.NewAtoms([]string{ "a", "c", "d" })),
     tr.NewTransaction(a.NewAtoms([]string{ "d", "e" })),
     tr.NewTransaction(a.NewAtoms([]string{ "d", "e", "f" })),
+    tr.NewTransaction(a.NewAtoms([]string{ "h", "e", "l", "l", "o", " ", "w", "o", "r", "l", "d" })),
+    tr.NewTransaction(a.NewAtoms([]string{ "h", "e", "l", "l", "o" })),
+    tr.NewTransaction(a.NewAtoms([]string{ "w", "o", "r", "l", "d" })),
   }
 
   input   := io.NewMemoryInput(&trans)
   output  := io.NewMemoryOutput()
   process := NewProcess(input, output, 2.6)
   process.Build()
+  //cluster.Print()
 }
